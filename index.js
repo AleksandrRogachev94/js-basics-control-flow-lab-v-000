bd092a4dba046220fed536cf0d2635aafa6eb94b
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-02 16:16:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let distance;
  function scuberGreetingForFeet(distance){
    if (distance <= 400){
      console.log("This one is on me!");
-   }
- } else if (distance > 2000) {
+   } else if (distance > 2000) {
    console.log("I will gladly take your thirty bucks.");
- }
+  }
+}
